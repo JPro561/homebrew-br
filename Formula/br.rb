c725ae5a3902cec2810965ebbd32299c2210bf5c
JProMi63 <clevertonjuniorsantiago@gmail.com>
2026-01-02 21:18:39 -0300

    Agora vai dá

--- Formula/br.rb
+++ Formula/br.rb
@@ -1,8 +1,8 @@
 class Br < Formula
     desc "Linguagem BR (Português / Brasileiro)"
     homepage "https://github.com/JPro561/BR"
-    url "https://github.com/JPro561/BR/archive/refs/tags/v0.1.0.tar.gz"
-    sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+    url "https://github.com/JPro561/BR/archive/refs/tags/v1.0.0.tar.gz"
+    sha256 "13e93b94aa50fe72c2651b877072b3a095538ed099626ae5d7a9b81f24b9d3af"
 
     depends_on "node"
 
