319b4bb06ef295af60e534526e0bb246039112be
JProMi63 <clevertonjuniorsantiago@gmail.com>
2026-01-02 21:53:23 -0300

    Fix sha256 for v1.0.2

--- Formula/br.rb
+++ Formula/br.rb
@@ -2,7 +2,7 @@ class Br < Formula
     desc "Linguagem BR (Português / Brasileiro)"
     homepage "https://github.com/JPro561/BR"
     url "https://github.com/JPro561/BR/archive/refs/tags/v1.0.2.tar.gz"
-    sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+    sha256 "6b960edd9706ddad34e2d46e60c8952cc83abb1c312181e8a397dad1a0d691ad"
 
     depends_on "node"
 
